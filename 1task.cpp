54b98d290e138d8b6cd326111a28f6e98271768e
HaoFlai <vinokurovataisia7@gmail.com>
2026-06-17 17:46:55 +0800

    Add files via upload

--- 1task.cpp
+++ 1task.cpp
@@ -1,31 +1,33 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 int main(){
     int n = 0;
     std::cin >> n;
-    int* ans = new int[n];
+    int** matrix = new int*[n];
+    int** dp = new int*[n];
 
     int l = 0;
     int sum = 0;
 
     for(int i = 0; i < n; i++) {
-        int mn = 10000;
         l++;
-        int* mas = new int[l];
+        matrix[i] = new int[l]; 
+        dp[i] = new int[l];
         for(int j = 0; j < l; j++) {
-            std::cin >> mas[j];
-            if(mas[j] < mn) {
-                mn = mas[j];
-            }
+            std::cin >> matrix[i][j];
         }
-        sum += mn;
-        ans[i] = mn;
-        delete[] mas;
     }
-    std::cout << sum << std::endl;
-    for(int i = 0; i < n; i++) {
-        std::cout << ans[i] << " ";
+
+    for(int j = 0; j < n; j++) {
+        dp[n-1][j] = matrix[n-1][j];
     }
-    delete[] ans;
+
+    for(int i = n - 2; i >= 0; i--) {
+        for(int j = 0; j <= i; j++) {
+            dp[i][j] = matrix[i][j] + std::min(dp[i+1][j], dp[i+1][j+1]);
+        }
+    }
+    std::cout << dp[0][0] << std::endl;
 }
